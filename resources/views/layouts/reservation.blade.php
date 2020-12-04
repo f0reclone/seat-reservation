868e035c9788f9f49ac1994c0da367938521d964
Andreas Neufeld <andreas.neufeld@aboutyou.com>
2020-12-05 00:19:47 +0100

    remove form, add current information, remove auth

--- resources/views/layouts/reservation.blade.php
+++ resources/views/layouts/reservation.blade.php
@@ -3,20 +3,18 @@
 
 @section('body')
     <body class="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover"
-          style="background-image:url('https://source.unsplash.com/PzhmEp_aDU4');">
+          style="background-image:url('/media/background.jpg');">
 
 
-    <div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
+    <div class="max-w-2xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-16 lg:my-0">
 
         <!--Main Col-->
         <div id="profile"
-             class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
+             class="w-full rounded-lg lg:rounded-l-lg shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
 
 
             <div class="p-4 md:p-12 text-center lg:text-left">
                 <!-- Image for mobile view-->
-                <div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center"
-                     style="background-image: url('https://source.unsplash.com/Wreb4Gwyf-M')"></div>
 
 
                 @yield('content')
@@ -27,13 +25,6 @@
                 </div>
             </div>
 
-        </div>
-
-        <!--Img Col-->
-        <div class="w-full lg:w-2/5">
-            <!-- Big profile image for side bar (desktop) -->
-            <img src="https://source.unsplash.com/Wreb4Gwyf-M"
-                 class="rounded-none lg:rounded-lg lg:rounded-l-none shadow-2xl hidden lg:block">
         </div>
 
     </div>
