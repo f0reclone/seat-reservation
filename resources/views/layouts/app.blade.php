81f9db5527592dc14bb142f4fdf3093d224773d5
Andreas Neufeld <andreas.neufeld@aboutyou.com>
2020-12-11 12:36:19 +0100

    remove jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,7 @@
     <title>{{ isset($metaDescription) ? $metaDescription : config('content.defaults.title') }}</title>
     <meta name="description"
           content="{{ isset($metaDescription) ? $metaDescription : config('content.defaults.description') }}">
-    <script
-            src="https://code.jquery.com/jquery-3.5.1.min.js"
-            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-            crossorigin="anonymous"></script>
     <title>Adventssingen 2020 Livestream</title>
-
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Dancing+Script:wght@700">
 
     <!-- Browser color -->
     <!-- Chrome, Firefox OS and Opera -->
@@ -27,7 +20,9 @@
     <!-- iOS Safari -->
     <meta name="apple-mobile-web-app-status-bar-style" content="#1a3a4f">
 
-@yield('styles.before')
+    @yield('styles.before')
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dancing+Script:wght@700">
+
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
 
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css">
